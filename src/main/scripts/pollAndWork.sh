faa5918e38cd82cf7104888593ad32462beaff2d
Mikis Seth Sørensen <mss@statsbiblioteket.dk>
2013-11-07 16:52:56 +0100

    Added logback config file option

--- src/main/scripts/pollAndWork.sh
+++ src/main/scripts/pollAndWork.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR="$(dirname "$0")"
 
-java -classpath "$SCRIPT_DIR/../lib/*" dk.statsbiblioteket.newspaper.bitrepository.ingester.BitrepositoryIngesterExecutable -c $SCRIPT_DIR/../conf/config.properties
+java -classpath "$SCRIPT_DIR/../lib/*" -Dlogback.configurationFile=$SCRIPT_DIR/../conf/logback.xml dk.statsbiblioteket.newspaper.bitrepository.ingester.BitrepositoryIngesterExecutable -c $SCRIPT_DIR/../conf/config.properties
