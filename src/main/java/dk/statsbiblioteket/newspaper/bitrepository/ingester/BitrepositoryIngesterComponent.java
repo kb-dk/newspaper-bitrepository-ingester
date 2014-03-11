9d2e35dda9bb4cbe13e8d06fa8a481a3b7267ffb
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2014-03-11 13:46:47 +0100

    Update to match new framework

--- src/main/java/dk/statsbiblioteket/newspaper/bitrepository/ingester/BitrepositoryIngesterComponent.java
+++ src/main/java/dk/statsbiblioteket/newspaper/bitrepository/ingester/BitrepositoryIngesterComponent.java
@@ -16,6 +16,7 @@ import dk.statsbiblioteket.medieplatform.autonomous.AbstractRunnableComponent;
 import dk.statsbiblioteket.medieplatform.autonomous.Batch;
 import dk.statsbiblioteket.medieplatform.autonomous.ConfigConstants;
 import dk.statsbiblioteket.medieplatform.autonomous.ResultCollector;
+import dk.statsbiblioteket.medieplatform.autonomous.SBOIBasedAbstractRunnableComponent;
 import dk.statsbiblioteket.medieplatform.autonomous.iterator.bitrepository.IngesterConfiguration;
 import dk.statsbiblioteket.medieplatform.autonomous.iterator.bitrepository.TreeIngester;
 
@@ -38,7 +39,7 @@ import org.slf4j.LoggerFactory;
 /**
  * Checks the directory structure of a batch. This should run both at Ninestars and at SB.
  */
-public class BitrepositoryIngesterComponent extends AbstractRunnableComponent {
+public class BitrepositoryIngesterComponent extends SBOIBasedAbstractRunnableComponent {
     private final Logger log = LoggerFactory.getLogger(getClass());
     public static final String COLLECTIONID_PROPERTY="bitrepository.ingester.collectionid";
     public static final String COMPONENTID_PROPERTY="bitrepository.ingester.componentid";
